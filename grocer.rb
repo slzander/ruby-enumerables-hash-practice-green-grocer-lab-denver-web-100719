605de84c9c9783d08760784dee9d8d18b9024a7c
Stacey Zander <staceyzander@gmail.com>
2019-09-18 01:48:46 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -24,7 +24,7 @@ def apply_coupons(cart, coupons)
     if cart[item] && cart[item][:count] >= coupon[:num] && !cart["#{item} W/COUPON"]
       cart["#{item} W/COUPON"] = {price: coupon[:cost] / coupon[:num], clearance: cart[item][:clearance], count: coupon[:num]
       cart[:item][:count] -= coupon[:num]
-    else if cart[item] && coupon[item][:count] >= coupon[:num] && cart["#{item} W/COUPON"][:count] += coupon[:num]
+    elsif cart[item] && coupon[item][:count] >= coupon[:num] && cart["#{item} W/COUPON"][:count] += coupon[:num]
       cart[item][:count] -= coupon[:num]
 
     end
